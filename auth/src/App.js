52ceb9dd4990d988e94224610fea5d6404967306
Maciej Marczak <maciejmarczak@hotmail.com>
2018-12-19 07:58:28 +0100

    initial commit

---
+++ auth/src/App.js
@@ -0,0 +1,25 @@
+import firebase from '@firebase/app';
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
+
+class App extends Component {
+
+	componentWillMount() {
+		/*firebase.initializeApp({
+			
+		});*/
+	}
+
+	render() {
+		return (
+			<View>
+				<Header headerText="Authentication" />
+				<LoginForm />
+			</View>
+		);
+	}
+}
+
+export default App;
